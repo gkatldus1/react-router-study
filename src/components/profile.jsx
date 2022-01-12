f13841d6de91b76cdb61a9abeb7e724ab0dda507
Siyeon <gkatldus1@gmail.com>
2022-01-12 22:01:16 +0900

    react-router-study

---
+++ src/components/profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1>Profile</h1>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Go to Home
+      </button>
+    </>
+  );
+};
+
+export default Profile;
